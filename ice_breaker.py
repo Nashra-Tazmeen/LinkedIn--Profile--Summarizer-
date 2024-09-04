81e5bbb8413673378fb19c961f95eac3656aed4e
Nashra Tazmeen <nashra2024@gmail.com>
2024-09-04 22:22:18 +0530

    commit

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,30 +1,31 @@
-     
-# import os
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI  
+from langchain.chains import LLMChain
 
-# from groq import Groq
+from third_parties.linkedin import scrape_linkedin_profile
 
-# client = Groq(
-#     api_key=os.environ.get("GROQ_API_KEY"),  
-# )
+if __name__ == "__main__":
+    load_dotenv()
 
-# chat_completion = client.chat.completions.create(
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "Explain the importance of fast language models",   
-#         }
-#     ],
-#     model="llama3-8b-8192",
-# )
+    print("Hello LangChain")
 
-# print(chat_completion.choices[0].message.content)
+    summary_template = """
+    given the Linkedin information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
 
-from groq import Groq
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
 
-client = Groq(api_key='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')
-response = client.chat.completions.create(
-    messages=[{"role": "user", "content": "Hello"}],     
-    model="llama3-8b-8192"
-)
-print(response)
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
+    )
+    res = chain.invoke(input={"information": linkedin_data})
+
+    print(res)
